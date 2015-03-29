6f3634031499670c91aba8576978f479a60d49d4
Mark <mark.hess2@gmail.com>
2015-03-29 08:39:18 -0500

    Minor Updates

---
+++ Part1-Q4_Strongly_Connected_Component/Assignment_4_SCC.py
@@ -0,0 +1,11 @@
+"""
+Algorithms: Design and Analysis Part 1
+Stanford University
+
+Programming Question 4
+Strongly Connected Components (Compute Largest 5)
+Depth-First Search SCC for directed graph
+Kosaraju's Two-Pass Algorithm
+"""
+
+
