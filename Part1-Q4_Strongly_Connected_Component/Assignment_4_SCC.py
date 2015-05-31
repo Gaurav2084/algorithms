c5ada7d71b78d780e9191b9c8854f786966b0e97
Mark <mark.hess2@gmail.com>
2015-05-31 13:55:22 -0500

    Update DFS Loops
    Updated DFS, DFS loop and added test graphs.

--- Part1-Q4_Strongly_Connected_Component/Assignment_4_SCC.py
+++ Part1-Q4_Strongly_Connected_Component/Assignment_4_SCC.py
@@ -8,7 +8,7 @@
 
 import urllib2
 
-# global name of file location
+# globals
 SCC_URL = "http://spark-public.s3.amazonaws.com/algo1/programming_prob/SCC.txt"
 SCC_LOCAL = "/Users/Hyperion/Desktop/SCC.txt"
 
@@ -39,40 +39,73 @@ def load_file(filename):
     print "Load failed."
 
 
+def dfs(graph, node):
+    """
+    Depth-First Search through each component
+
+    Input:  graph (dict)
+            node (int)
+    """
+    global time
+    # add node to explored list and notate who its leader is
+    explored.append(node)
+    leaders[node] = lead
+    # loop through all of edges coming from node, recurse unvisited
+    for edge in graph[node]:
+        if edge not in explored:
+            dfs(graph, node)
+    # increase time step 1 and set nodes time step to new time
+    time += 1
+    fTimes[node] = time
+
+
 def dfs_loop(graph):
     """
     Loop used to calculate strongly connected component
 
     input: graph (dict)
     """
-    time = 0  # finishing times for nodes, first pass
-    lead = None  # leaders for nodes, second pass
-    explored = []
     max_node = max(graph)
     for node in xrange(max_node, -1, -1):
         if node not in explored:
             lead = node
-            dfs(graph, node, time, lead)
-
-
-def dfs(graph, explored, node, times, lead):
-    """
-    Depth-First Search through each component
-
-    Input:  graph (dict)
-            node (int)
-            times (int)
-            lead (int)
-    """
-    explored.append(node)
+            dfs(graph, node)
 
 
 def main(filename):
+    global time  # finishing times for nodes, first pass
+    global lead  # leaders for nodes, second pass
+    global explored = []  # explored nodes
+    global leaders = {}
+    global fTimes = {}  # associated leaders and finishing times
     load_file(filename)
 
 
 def tester():
-
+    graph_one = {
+        1: [7],
+        2: [5],
+        3: [9],
+        4: [1],
+        5: [8],
+        6: [3],
+        7: [9],
+        8: [2],
+        9: [6]
+    }
+    graph_two = {
+        1: [2],
+        2: [3, 4],
+        3: [1, 8, 9],
+        4: [5, 6],
+        5: [6],
+        6: [7],
+        7: [5, 7],
+        8: [5, 10, 11],
+        9: [8],
+        10: [9],
+        11: [10]
+    }
     pass
 
 tester()
