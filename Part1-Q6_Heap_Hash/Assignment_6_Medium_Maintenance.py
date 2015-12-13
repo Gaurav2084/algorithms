58ad8e8e47ab955e3347bc063c120285c05df7dd
Mark <mark.hess2@gmail.com>
2015-12-13 11:23:29 -0600

    Add logic for median maintenance

--- Part1-Q6_Heap_Hash/Assignment_6_Medium_Maintenance.py
+++ Part1-Q6_Heap_Hash/Assignment_6_Medium_Maintenance.py
@@ -2,8 +2,49 @@
 # Stanford University
 
 # Programming Question 6 - Part 2
+# Find median for set of numbers as they are given 1 by 1
+# Median Maintenance utilizing heaps
 
 import urllib2
+import heapq
 
+
+# constants
 MEDIAN_URL = "http://spark-public.s3.amazonaws.com/" + \
              "algo1/programming_prob/Median.txt"
+NUM_OF_INTS = 10000
+
+
+def stream_file(filename):
+    """
+    Generates integers from a give file location URL.
+    :param filename: file location (string)
+    :return: yeild 1 integer from file as called
+    """
+    file_object = urllib2.urlopen(filename)
+    for line in file_object:
+        yield int(line)
+    file_object.close()
+
+
+def main(num_ints, filename):
+    """
+    Main program logic for median maintenance algorithm.
+    :param num_ints: number of integers to modulo with (int)
+    :param filename: file location of integers (string)
+    :return: sum of ints modulo number of ints (int)
+    """
+    median_sums = 0
+    low_heap = heapq
+    high_heap = heapq
+
+    # loop through numbers from file
+    for number in stream_file(filename):
+        break
+
+    # return sum of all medians
+    return median_sums % num_ints
+
+
+# print to console result of the algorithm on given file
+print "Answer is: " + str(main(NUM_OF_INTS, MEDIAN_URL))
